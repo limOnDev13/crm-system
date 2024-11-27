1d247c76eed2fa14eea2fd9eb9fb18c3c935a075
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 15:37:49 +0500

    Register path products/new/

--- crm/services/urls.py
+++ crm/services/urls.py
@@ -5,12 +5,14 @@ from .views import (
     ServiceDetailView,
     ServiceUpdateView,
     ServiceDeleteView,
+    ServiceCreateView,
 )
 
 app_name = "services"
 
 urlpatterns = [
     path("products/", ServicesListView.as_view(), name="services_list"),
+    path("products/new/", ServiceCreateView.as_view(), name="service_create"),
     path("products/<int:pk>/", ServiceDetailView.as_view(), name="service_detail"),
     path("products/<int:pk>/edit/", ServiceUpdateView.as_view(), name="service_edit"),
     path("products/<int:pk>/delete/", ServiceDeleteView.as_view(), name="service_delete"),
