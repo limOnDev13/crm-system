802c80c337a7b6e6ce1a2842d665fd28f505f0c5
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 12:48:46 +0500

    Register ServiceViewSet paths

--- crm/services/urls.py
+++ crm/services/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import ServiceViewSet
 
 app_name = "services"
 
-urlpatterns = [
+routers = DefaultRouter()
+routers.register("services", ServiceViewSet)
 
+urlpatterns = [
+    path("api/", include(routers.urls)),
 ]
