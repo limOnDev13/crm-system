3bedff0ae04f882e76262295d1e85761b8f482df
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 15:16:08 +0500

    Fix class Service (change field price to cost)

--- crm/services/models.py
+++ crm/services/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
-    price = models.DecimalField(null=False, default=0, max_digits=8, decimal_places=2)
+    cost = models.DecimalField(null=False, default=0, max_digits=8, decimal_places=2)
