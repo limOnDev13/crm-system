cd1d23096af338c2e193c971e1d5c4be38b0dc11
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 12:06:29 +0500

    Add services app paths

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('auth/', include('myauth.urls'))
+    path('auth/', include('myauth.urls')),
+    path('services/', include('services.urls')),
 ]
