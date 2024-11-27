a52c974e8910f2609ad5d0226113ec55b9e28a0d
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 15:24:55 +0500

    Register path products/<pk>/edit/

--- crm/services/urls.py
+++ crm/services/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ServicesListView, ServiceDetailView
+from .views import ServicesListView, ServiceDetailView, ServiceUpdateView
 
 app_name = "services"
 
 urlpatterns = [
     path("products/", ServicesListView.as_view(), name="services_list"),
     path("products/<int:pk>/", ServiceDetailView.as_view(), name="service_detail"),
+    path("products/<int:pk>/edit/", ServiceUpdateView.as_view(), name="service_edit"),
 ]
