eef90ff0a0b7cc55c37386fb0bfaa9d602de50af
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 15:17:14 +0500

    Add path products/<pk>; change path services/ to products/

--- crm/services/urls.py
+++ crm/services/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ServicesListView
+from .views import ServicesListView, ServiceDetailView
 
 app_name = "services"
 
 urlpatterns = [
-    path("services/", ServicesListView.as_view(), name="services_list"),
+    path("products/", ServicesListView.as_view(), name="services_list"),
+    path("products/<int:pk>/", ServiceDetailView.as_view(), name="service_detail"),
 ]
