0b58f95006fa6afbf6c7c19b4957c805bd56fbd4
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 12:37:55 +0500

    Register and config rest_framework

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'myauth.apps.MyauthConfig',
-    'services.apps.ServicesConfig'
+    'services.apps.ServicesConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
