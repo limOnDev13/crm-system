017618b9a9cbfe9e309483506cca4864e27c09e7
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 15:15:57 +0500

    Fix class ServiceFactory (change field price to cost)

--- crm/services/factories.py
+++ crm/services/factories.py
@@ -7,11 +7,11 @@ from services.models import Service
 class ServiceFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Service  # Equivalent to ``model = myapp.models.User``
-        django_get_or_create = ("name", "description", "price")
+        django_get_or_create = ("name", "description", "cost")
 
     name = factory.faker.Faker("word")
     description = factory.faker.Faker("text")
-    price = factory.fuzzy.FuzzyFloat(0, 100, precision=4)
+    cost = factory.fuzzy.FuzzyFloat(0, 100, precision=4)
 
 
 if __name__ == "__main__":
