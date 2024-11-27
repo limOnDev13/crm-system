4d6ceb7e07298a00112130c93831ad8ed44b735c
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-27 14:28:22 +0500

    Create module create_services.py; create django command create_services for creating random services with factory_boy

---
+++ crm/services/management/commands/create_services.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+
+from services.factories import ServiceFactory
+
+
+class Command(BaseCommand):
+    help = "Create random services."
+
+    def add_arguments(self, parser):
+        parser.add_argument("count", type=int, help="Count of services")
+
+    def handle(self, *args, **kwargs):
+        count = kwargs["count"]
+        self.stdout.write(f"Creating {count} random products...")
+
+        for num in range(count):
+            service = ServiceFactory.create()
+            self.stdout.write(
+                f"Create #{num} service:\n"
+                f"pk={service.pk} name={service.name} price={service.price} description:\n{service.description}\n"
+            )
+            service.save()
