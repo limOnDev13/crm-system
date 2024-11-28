79821e6122bd34fd8beb6930fba76564948a6008
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 13:44:34 +0500

    Add method __str__ for model Service

--- crm/services/models.py
+++ crm/services/models.py
@@ -7,3 +7,6 @@ class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     cost = models.DecimalField(null=False, default=0, max_digits=8, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.pk})"
