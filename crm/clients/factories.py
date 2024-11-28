935099e721be36403a63728742959c94437aba03
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 16:40:51 +0500

    Minor fixes

--- crm/clients/factories.py
+++ crm/clients/factories.py
@@ -1,7 +1,7 @@
 import factory.fuzzy
+from advertising.factories import AdvertisingFactory
 
 from .models import Lead
-from advertising.factories import AdvertisingFactory
 
 
 class LeadFactory(factory.django.DjangoModelFactory):
@@ -9,16 +9,10 @@ class LeadFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Lead
-        django_get_or_create = (
-            "first_name",
-            "second_name",
-            "phone",
-            "email",
-            "ads"
-        )
+        django_get_or_create = ("first_name", "second_name", "phone", "email", "ads")
 
     first_name = factory.faker.Faker("first_name")
     second_name = factory.faker.Faker("last_name")
-    phone = factory.Sequence(lambda n: '+7 (999) 000 %04d' % n)
+    phone = factory.Sequence(lambda n: "+7 (999) 000 %04d" % n)
     email = factory.faker.Faker("email")
     ads = factory.SubFactory(AdvertisingFactory)
