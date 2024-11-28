e12225e52ae7962804015124ebb71a0458b8ac39
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 14:08:38 +0500

    Create TestCase class ServicesListViewTest for testing AdvertisingListView

--- crm/advertising/tests.py
+++ crm/advertising/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import random
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from factory.faker import Faker
+
+from .factories import AdvertisingFactory
+from .models import Advertising
+
+
+class AdvertisingListViewTest(TestCase):
+    """Test case class for testing AdvertisingListView."""
+
+    fixtures = [
+        "services-fixtures.json",
+        "ads-fixtures.json",
+    ]
+
+    def test_get_list_advertising(self):
+        """Test getting list of advertising"""
+        response = self.client.get(reverse("advertising:ads_list"))
+
+        self.assertQuerySetEqual(
+            qs=Advertising.objects.order_by("pk").all(),
+            values=(s.pk for s in response.context["ads"]),
+            transform=lambda p: p.pk,
+        )
