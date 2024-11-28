bc97969b378fd84cf82a75c659925a7bc67b4ec9
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 18:31:30 +0500

    Create test case for LeadDetailView

--- crm/clients/tests.py
+++ crm/clients/tests.py
@@ -26,3 +26,21 @@ class LeadsListViewTest(TestCase):
             values=(s.pk for s in response.context["leads"]),
             transform=lambda p: p.pk,
         )
+
+
+class LeadsDetailViewTest(TestCase):
+    """Test case class for testing LeadDetailView."""
+
+    def setUp(self):
+        self.lead = LeadFactory.create()
+
+    def tearDown(self):
+        self.lead.delete()
+
+    def test_get_details_about_lead(self):
+        """Test getting details about the lead."""
+        response = self.client.get(
+            reverse("clients:leads_detail", kwargs={"pk": self.lead.pk})
+        )
+
+        self.assertEqual(response.status_code, 200)
