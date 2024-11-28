fbb75c2e11377825c8ea534704ba24dc2c160d53
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 16:10:10 +0500

    Create model Lead

--- crm/clients/models.py
+++ crm/clients/models.py
@@ -1,3 +1,18 @@
+from advertising.models import Advertising
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+    """ORM view of the PotClient (potential clients) table."""
+
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    second_name = models.CharField(max_length=100, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=False, blank=False, unique=True)
+    email = models.EmailField(null=False, unique=True)
+    ads = models.ForeignKey(
+        Advertising,
+        null=True,
+        on_delete=models.SET_NULL,
+        help_text="the advertising campaign from which the"
+                  " lead learned about the service",
+    )
