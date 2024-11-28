6cf03cd1848abed173e9113c1e9ca20ed1207312
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 19:55:27 +0500

    Configure static and media output in DEBUG mode

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -126,10 +126,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = []
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'upload'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
