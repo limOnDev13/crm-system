95f19566a13d75b21d36d096dc50e45fc71e85e7
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 13:50:29 +0500

    Register path ads/<int:pk>/delete/ for AdvertisingDeleteView

--- crm/advertising/urls.py
+++ crm/advertising/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from .views import AdvertisingDetailView, AdvertisingListView, AdvertisingUpdateView
+from .views import (
+    AdvertisingDeleteView,
+    AdvertisingDetailView,
+    AdvertisingListView,
+    AdvertisingUpdateView,
+)
 
 app_name = "advertising"
 
@@ -8,4 +13,5 @@ urlpatterns = [
     path("ads/", AdvertisingListView.as_view(), name="ads_list"),
     path("ads/<int:pk>/", AdvertisingDetailView.as_view(), name="ads_detail"),
     path("ads/<int:pk>/edit/", AdvertisingUpdateView.as_view(), name="ads_update"),
+    path("ads/<int:pk>/delete/", AdvertisingDeleteView.as_view(), name="ads_delete"),
 ]
