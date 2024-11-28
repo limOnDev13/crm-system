1afc93ba64614545d6316a0a02ce82010126f9ae
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 17:47:54 +0500

    Register path leads/new/ for LeadCreateView

--- crm/clients/urls.py
+++ crm/clients/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import LeadDeleteView, LeadDetailView, LeadsListView, LeadUpdateView
+from .views import (
+    LeadCreateView,
+    LeadDeleteView,
+    LeadDetailView,
+    LeadsListView,
+    LeadUpdateView,
+)
 
 app_name = "clients"
 
 urlpatterns = [
     path("leads/", LeadsListView.as_view(), name="leads_list"),
+    path("leads/new/", LeadCreateView.as_view(), name="leads_create"),
     path("leads/<int:pk>/", LeadDetailView.as_view(), name="leads_detail"),
     path("leads/<int:pk>/edit/", LeadUpdateView.as_view(), name="leads_edit"),
     path("leads/<int:pk>/delete/", LeadDeleteView.as_view(), name="leads_delete"),
