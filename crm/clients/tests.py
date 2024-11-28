ab6d62be41dd334738df01c22d0b13d13b800748
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 18:06:49 +0500

    Create test case for LeadsListView

--- crm/clients/tests.py
+++ crm/clients/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import random
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from factory.faker import Faker
+
+from .factories import LeadFactory
+from .models import Lead
+
+
+class LeadsListViewTest(TestCase):
+    """Test case class for testing LeadsListView."""
+
+    fixtures = [
+        "services-fixtures.json",
+        "ads-fixtures.json",
+        "leads-fixtures.json",
+    ]
+
+    def test_get_list_leads(self):
+        """Test getting list of leads"""
+        response = self.client.get(reverse("clients:leads_list"))
+
+        self.assertQuerySetEqual(
+            qs=Lead.objects.order_by("pk").all(),
+            values=(s.pk for s in response.context["leads"]),
+            transform=lambda p: p.pk,
+        )
