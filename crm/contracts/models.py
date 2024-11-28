fcf4227eb67cb927a5af79536315d627dc26b4a8
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 19:55:56 +0500

    Create model Contract

--- crm/contracts/models.py
+++ crm/contracts/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from datetime import timedelta
 
-# Create your models here.
+from django.db import models
+from services.models import Service
+
+
+class Contract(models.Model):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        unique=True,
+        help_text="the unique name of the contract",
+    )
+    product = models.ForeignKey(
+        Service, on_delete=models.CASCADE, null=False, help_text="the service provided"
+    )
+    doc = models.FileField(
+        null=False,
+        upload_to="contracts",
+        help_text="the file with the contract document",
+    )
+    date = models.DateField(
+        null=False, auto_now_add=True, help_text="date of conclusion of the contract"
+    )
+    duration = models.DurationField(
+        null=False, default=timedelta(days=1), help_text="duration of the contract"
+    )
+    cost = models.DecimalField(null=False, default=0, max_digits=8, decimal_places=2)
