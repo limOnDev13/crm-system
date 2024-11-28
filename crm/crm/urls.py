53dd3d8916e29b4e4199b018626ab276bd51da5c
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 15:48:15 +0500

    Register app clients paths

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("", include("advertising.urls")),
     path("admin/", admin.site.urls),
     path("auth/", include("myauth.urls")),
+    path("clients/", include("clients.urls")),
 ]
