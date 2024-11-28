dfed1f20502cca5c6fbc241e7d3496a0b49d9823
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 13:43:44 +0500

    Replace field model to queryset in class AdvertisingDetailView; replace field model to queryset in class AdvertisingListView; create UpdateView class AdvertisingUpdateView

--- crm/advertising/views.py
+++ crm/advertising/views.py
@@ -1,8 +1,9 @@
+from django.urls import reverse
 from django.views.generic import (
-    DetailView,
-    ListView,
     CreateView,
     DeleteView,
+    DetailView,
+    ListView,
     UpdateView,
 )
 
@@ -13,7 +14,7 @@ class AdvertisingListView(ListView):
     """ListView class for getting list of advertising."""
 
     template_name = "advertising/ads-list.html"
-    model = Advertising
+    queryset = Advertising.objects.select_related("product")
     context_object_name = "ads"
 
 
@@ -21,4 +22,15 @@ class AdvertisingDetailView(DetailView):
     """DetailView class for getting details about the advertising."""
 
     template_name = "advertising/ads-detail.html"
+    queryset = Advertising.objects.select_related("product")
+
+
+class AdvertisingUpdateView(UpdateView):
+    """UpdateView class for updating the service."""
+
+    template_name = "advertising/ads-edit.html"
     model = Advertising
+    fields = "name", "channel", "budget", "product"
+
+    def get_success_url(self):
+        return reverse("advertising:ads_detail", kwargs={"pk": self.object.pk})
