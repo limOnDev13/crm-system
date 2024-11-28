f28ed3baff11a03ef630ec6ed155fd51e96b52a5
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-28 13:45:11 +0500

    Register path ads/<int:pk>/edit for AdvertisingUpdateView

--- crm/advertising/urls.py
+++ crm/advertising/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from .views import (
-    AdvertisingListView,
-    AdvertisingDetailView,
-)
+from .views import AdvertisingDetailView, AdvertisingListView, AdvertisingUpdateView
 
 app_name = "advertising"
 
 urlpatterns = [
     path("ads/", AdvertisingListView.as_view(), name="ads_list"),
     path("ads/<int:pk>/", AdvertisingDetailView.as_view(), name="ads_detail"),
+    path("ads/<int:pk>/edit/", AdvertisingUpdateView.as_view(), name="ads_update"),
 ]
