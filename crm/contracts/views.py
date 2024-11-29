450817c3ce4f26b8717a0715a46452d300cad00c
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-29 10:17:39 +0500

    Create ListView class ContractListView

--- crm/contracts/views.py
+++ crm/contracts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-# Create your views here.
+from .models import Contract
+
+
+class ContractListView(ListView):
+    """ListView class for getting list of contracts."""
+
+    template_name = "contracts/contracts-list.html"
+    queryset = Contract.objects.select_related("product")
+    context_object_name = "contracts"
