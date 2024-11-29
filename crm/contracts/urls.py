541537623ba5e39122e50c0846703dc8a4e62fed
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-29 14:01:47 +0500

    Add urlpattern contracts/<int:pk>/edit/ for ContractUpdateView

--- crm/contracts/urls.py
+++ crm/contracts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ContractListView, ContractDetailView
+from .views import ContractListView, ContractDetailView, ContractUpdateView
 
 app_name = "contracts"
 
 urlpatterns = [
     path("contracts/", ContractListView.as_view(), name="contracts_list"),
     path("contracts/<int:pk>/", ContractDetailView.as_view(), name="contract_detail"),
+    path("contracts/<int:pk>/edit/", ContractUpdateView.as_view(), name="contract_edit"),
 ]
