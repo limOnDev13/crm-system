298379c804d6e29b0040dfaa1c911567651e686b
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-29 13:49:49 +0500

    Add urlpattern contracts/<int:pk>/ for ContractDetailView

--- crm/contracts/urls.py
+++ crm/contracts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ContractListView
+from .views import ContractListView, ContractDetailView
 
 app_name = "contracts"
 
 urlpatterns = [
     path("contracts/", ContractListView.as_view(), name="contracts_list"),
+    path("contracts/<int:pk>/", ContractDetailView.as_view(), name="contract_detail"),
 ]
