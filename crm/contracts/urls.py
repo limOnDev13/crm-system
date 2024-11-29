675d965661e1a0b45cf14d2361646b1d177eaf9b
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-29 10:18:16 +0500

    Add path contracts/ for ContractListView

--- crm/contracts/urls.py
+++ crm/contracts/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from .views import ContractListView
+
 app_name = "contracts"
 
-urlpatterns = []
+urlpatterns = [
+    path("contracts/", ContractListView.as_view(), name="contracts_list"),
+]
