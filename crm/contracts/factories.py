748e93934a5e6418b04b78b4f55b82d6414bd754
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-30 10:20:03 +0500

    Rename func _create_empty_file to _create_test_file; add a line entry to the created file to generate non-empty files; fix field end_date

--- crm/contracts/factories.py
+++ crm/contracts/factories.py
@@ -1,6 +1,6 @@
 import os
 import random
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 from typing import Optional
 
 import factory.fuzzy
@@ -9,7 +9,7 @@ from services.factories import ServiceFactory
 from .models import Contract
 
 
-def _create_empty_file(
+def _create_test_file(
     file_dir: Optional[str] = None, filename: str = "empty_file.txt"
 ) -> str:
     """Create empty file for factory."""
@@ -18,8 +18,8 @@ def _create_empty_file(
     else:
         path = os.path.join(file_dir, filename)
 
-    with open(path, encoding="UTF-8", mode="w"):
-        pass
+    with open(path, encoding="UTF-8", mode="w") as file:
+        file.write("Some data")
 
     return path
 
@@ -33,6 +33,6 @@ class ContractFactory(factory.django.DjangoModelFactory):
 
     name = factory.faker.Faker("word")
     product = factory.SubFactory(ServiceFactory)
-    doc = factory.django.FileField(from_path=_create_empty_file())
+    doc = factory.django.FileField(from_path=_create_test_file())
     cost = factory.LazyAttribute(lambda x: round(random.uniform(0, 100), 2))
-    end_date = factory.LazyAttribute(lambda x: datetime.now() + timedelta(days=1))
+    end_date = factory.LazyAttribute(lambda x: date.today() + timedelta(days=1))
