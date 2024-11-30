92211deb926ed9d869495a76844c8cb05d1ae0cc
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-30 17:17:52 +0500

    Create model Customer

--- crm/clients/models.py
+++ crm/clients/models.py
@@ -1,11 +1,12 @@
 from advertising.models import Advertising
+from contracts.models import Contract
 from django.db import models
 
 from .validators import validate_phone
 
 
 class Lead(models.Model):
-    """ORM view of the PotClient (potential clients) table."""
+    """ORM view of the Lead (potential clients) table."""
 
     first_name = models.CharField(max_length=100, null=False, blank=False)
     second_name = models.CharField(max_length=100, null=False, blank=False)
@@ -20,3 +21,10 @@ class Lead(models.Model):
         help_text="the advertising campaign from which the"
         " lead learned about the service",
     )
+
+
+class Customer(models.Model):
+    """ORM view of the customer (potential clients) table."""
+
+    lead = models.OneToOneField(Lead, on_delete=models.CASCADE)
+    contract = models.OneToOneField(Contract, on_delete=models.CASCADE)
