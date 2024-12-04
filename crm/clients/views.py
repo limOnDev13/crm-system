f36d44576d1ba45d0f427f39b207ef3fbbd103ab
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-04 17:39:30 +0500

    Create FormView class for creating a new customer; create DeleteView class for deleting the customer; create view func update_customer for updating the customer

--- crm/clients/views.py
+++ crm/clients/views.py
@@ -1,13 +1,22 @@
+from logging import getLogger
+
+from contracts.models import Contract
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
     CreateView,
     DeleteView,
     DetailView,
+    FormView,
     ListView,
     UpdateView,
 )
 
+from .forms import CustomerForm
 from .models import Customer, Lead
+
+logger = getLogger()
 
 
 class LeadsListView(ListView):
@@ -66,3 +75,73 @@ class CustomerDetailView(DetailView):
 
     template_name = "clients/customers-detail.html"
     queryset = Customer.objects.select_related("lead").select_related("contract")
+
+
+class CustomerCreateView(FormView):
+    """CreateView class for creating a new customer."""
+
+    template_name = "clients/customers-create.html"
+    success_url = reverse_lazy("clients:customers_list")
+    form_class = CustomerForm
+
+    def form_valid(self, form: CustomerForm):
+        resp = super().form_valid(form)
+        lead_data, contract_data = form.get_data_from_customer_form()
+
+        lead = Lead.objects.create(**lead_data)
+        contract = Contract.objects.create(**contract_data)
+        Customer.objects.create(lead=lead, contract=contract)
+
+        return resp
+
+
+class CustomerDeleteView(DeleteView):
+    """DeleteView for deleting the customer."""
+
+    template_name = "clients/customers-delete.html"
+    queryset = Customer.objects.select_related("lead").select_related("contract")
+    success_url = reverse_lazy("clients:customers_list")
+
+
+def update_customer(request: HttpRequest, pk: int) -> HttpResponse:
+    """View func for updating the customer."""
+    if request.method == "POST":
+        form = CustomerForm(request.POST, request.FILES)
+        if form.is_valid():
+            lead_data, contract_data = form.get_data_from_customer_form()
+            customer = Customer.objects.get(pk=pk)
+            lead = customer.lead
+            contract = customer.contract
+
+            for key, value in lead_data.items():
+                setattr(lead, key, value)
+            for key, value in contract_data.items():
+                setattr(contract, key, value)
+
+            customer.lead.save()
+            customer.contract.save()
+
+            url = reverse("clients:customers_detail", kwargs={"pk": pk})
+            return redirect(url)
+
+        return redirect(request.path)
+
+    customer = (
+        Customer.objects.select_related("lead").select_related("contract").get(pk=pk)
+    )
+    form = CustomerForm(
+        initial={
+            "first_name": customer.lead.first_name,
+            "last_name": customer.lead.last_name,
+            "phone": customer.lead.phone,
+            "email": customer.lead.email,
+            "ads": customer.lead.ads,
+            "name": customer.contract.name,
+            "product": customer.contract.product,
+            "doc": customer.contract.doc,
+            "end_date": customer.contract.end_date,
+            "cost": customer.contract.cost,
+        }
+    )
+    context = {"form": form, "object": customer}
+    return render(request, "clients/customers-edit.html", context=context)
