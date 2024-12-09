60bfd7c9ff120376af70f24484679af0ad07232d
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-09 13:33:11 +0500

    Fix settings for static files

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -138,9 +138,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATICFILES_DIRS = []
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'upload'
