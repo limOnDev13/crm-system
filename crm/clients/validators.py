fd35cf4c463518507dcf71df8863d57bcd6d7377
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-09 13:37:53 +0500

    Rename validator validate_phone to validate_phone_format; add validators validate_unique_phone, validate_unique_contract_name, validate_unique_email

--- crm/clients/validators.py
+++ crm/clients/validators.py
@@ -1,12 +1,37 @@
 import re
+from typing import Optional
 
+from contracts.models import Contract
 from django.core.exceptions import ValidationError
 
+from .models import Lead
 
-def validate_phone(value: str):
-    """Phone validator."""
+
+def validate_phone_format(value: str) -> None:
+    """Validate phone format (must be like +7 (999) 000 0000)."""
     # phone must have format +7 (999) 000 0000
     if not re.match(r"\+7 \(\d{3}?\) \d{3}? \d{4}?", value):
         raise ValidationError(
             "Phone must have format +7 (999) 000 0000", params={"phone": value}
         )
+
+
+def validate_unique_phone(value: str) -> None:
+    """Validate that phone is unique."""
+    lead = Lead.objects.filter(phone=value).first()
+    if lead is not None:
+        raise ValidationError("Phone already exists", params={"phone": value})
+
+
+def validate_unique_contract_name(value: str) -> None:
+    """Validate that contract name is unique."""
+    contract = Contract.objects.filter(name=value).first()
+    if contract is not None:
+        raise ValidationError("Contract name already exists", params={"name": value})
+
+
+def validate_unique_email(value: str) -> None:
+    """Validate that email is unique."""
+    lead: Optional[Lead] = Lead.objects.filter(email=value).first()
+    if lead is not None:
+        raise ValidationError("Email already exists", params={"email": value})
