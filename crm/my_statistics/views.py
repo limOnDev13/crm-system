57c2ec00ef359c9ed1207991121b3f8a8cd1e79e
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-12 19:15:14 +0500

    Create view func get_ads_statistics

--- crm/my_statistics/views.py
+++ crm/my_statistics/views.py
@@ -1,3 +1,14 @@
+from typing import List, Dict
+
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .business.statistics_logic import ads_statistics
+from .business.statistics_models import AdsStatistics
+
+
+def get_ads_statistics(request: HttpRequest) -> HttpResponse:
+    """View function for getting statistics on ads."""
+    statistics: List[AdsStatistics] = ads_statistics()
+    context: Dict[str, List[AdsStatistics]] = {"ads": statistics}
+    return render(request, "my_statistics/ads-statistic.html", context=context)
