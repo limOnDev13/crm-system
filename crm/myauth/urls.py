bb6d100fe39d9552475b675a45d1a2cddb479204
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-12 12:11:24 +0500

    Add urlpattern logout/ for LogoutView

--- crm/myauth/urls.py
+++ crm/myauth/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.views import LoginView
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
 
 app_name = "myauth"
 
@@ -11,5 +11,10 @@ urlpatterns = [
             redirect_authenticated_user=True,
         ),
         name="login",
-    )
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(next_page=reverse_lazy("myauth:login")),
+        name="logout"
+    ),
 ]
