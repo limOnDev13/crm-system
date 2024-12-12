c76fa6f30bb3a1711ca781129fd86e4ef1ad94ed
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-12 19:33:21 +0500

    Create view func get_total_statistics

--- crm/my_statistics/views.py
+++ crm/my_statistics/views.py
@@ -1,10 +1,10 @@
-from typing import List, Dict
+from typing import Dict, List
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from .business.statistics_logic import ads_statistics
-from .business.statistics_models import AdsStatistics
+from .business.statistics_logic import ads_statistics, total_statistics
+from .business.statistics_models import AdsStatistics, TotalStatistics
 
 
 def get_ads_statistics(request: HttpRequest) -> HttpResponse:
@@ -12,3 +12,10 @@ def get_ads_statistics(request: HttpRequest) -> HttpResponse:
     statistics: List[AdsStatistics] = ads_statistics()
     context: Dict[str, List[AdsStatistics]] = {"ads": statistics}
     return render(request, "my_statistics/ads-statistic.html", context=context)
+
+
+def get_total_statistics(request: HttpRequest) -> HttpResponse:
+    """View function for getting total statistics."""
+    statistics: TotalStatistics = total_statistics()
+    context: Dict[str, int] = statistics.to_dict()
+    return render(request, "my_statistics/index.html", context=context)
