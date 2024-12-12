fd49d92d409b52f0d0f6ba7fe4bcaa0de035f2a1
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-12-12 19:35:08 +0500

    Create class TotalStatistics

--- crm/my_statistics/business/statistics_models.py
+++ crm/my_statistics/business/statistics_models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Dict
 
 
 @dataclass
@@ -7,3 +8,19 @@ class AdsStatistics:
     leads_count: int
     customers_count: int
     profit: float
+
+
+@dataclass
+class TotalStatistics:
+    products_count: int
+    advertisements_count: int
+    leads_count: int
+    customers_count: int
+
+    def to_dict(self) -> Dict[str, int]:
+        return {
+            "products_count": self.products_count,
+            "advertisements_count": self.advertisements_count,
+            "leads_count": self.leads_count,
+            "customers_count": self.customers_count,
+        }
